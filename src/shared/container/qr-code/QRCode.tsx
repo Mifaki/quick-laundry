57d2e8f0e308998e07e5c6a3889277a643703b22
dhillrmdhn <fadhilrmdhn3010@gmail.com>
2024-12-11 00:32:58 +0700

    refactor: replace img tag with Image Next and add config to next.config

--- src/shared/container/qr-code/QRCode.tsx
+++ src/shared/container/qr-code/QRCode.tsx
@@ -1,10 +1,13 @@
+import Image from "next/image";
+
 const QRCode = ({ link }: { link: string }) => {
   return (
     <div className="flex items-center justify-center">
-      <img
+      <Image
         src={`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${link}`}
         alt="QR Code"
-        className="w-52"
+        width={225}
+        height={225}
       />
     </div>
   );
