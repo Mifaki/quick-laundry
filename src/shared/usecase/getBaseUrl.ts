7b20006e9b452c1227e933072b11893055348928
Ahmad Faiz Agustianto <107536877+Mifaki@users.noreply.github.com>
2024-12-11 19:46:04 +0700

    Build: Prepare for Webserver Deployment

--- src/shared/usecase/getBaseUrl.ts
+++ src/shared/usecase/getBaseUrl.ts
@@ -2,9 +2,9 @@ export default function getBaseUrl() {
   if (typeof window !== 'undefined')
     // browser should use relative path
     return '';
-  if (process.env.VERCEL_URL)
+  if (process.env.PROD_URL)
     // reference for vercel.com
-    return `https://${process.env.VERCEL_URL}`;
+    return `https://${process.env.PROD_URL}`;
   // assume localhost
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
